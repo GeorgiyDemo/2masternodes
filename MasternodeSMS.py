23dc22047d6619b0c57725724463934cd6a0c671
Georgiy Demenchuk <demenchuk.george@mail.ru>
2018-07-18 12:58:51 +0300

    SMS Alerts ready

--- MasternodeSMS.py
+++ MasternodeSMS.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
-from pytils import numeral
-import pymysql.cursors
-import string, vk, time, datetime, json, requests, urllib3, dateutil.parser
+import pymysql.cursors, time, json, requests, dateutil.parser
 
 #####Параметры MySQL######
 HOST = "HOST"            #
@@ -10,8 +8,12 @@ PASSWORD = "PASSWORD"    #
 DB = "DB"                #
 ##########################
 
-def SmsSend(sms_string):
-	print(sms_string)
+#Функция отправки сообщений по SMS
+def SmsSend(sms_string, sms_number, url_string):
+	sms = requests.get(url_string+"/sms/send?number="+sms_number+"&text="+sms_string+"&sign=SMS Aero&channel=DIRECT").json()
+	if sms["success"]==True:
+			return True
+	return False
 
 #Функция для взаимодействия с одним элементом БД
 def MySQLFetchOne(SQLString):
@@ -40,17 +42,19 @@ def MySQLWriter(SQLString):
 	    connection.close()
 
 def main():
-	while True:
-		MyMasternode = requests.get("https://api.2masternodes.com/api/coin/gbx/masternode/[MASTERNODE_NAME]").json()["royalty"][0]
-		print(MyMasternode)
 
+	SMSLogin = MySQLFetchOne("SELECT Val FROM AuthTable WHERE Auth='SMSAeroLogin'")["Val"]
+	SMSAPIKey = MySQLFetchOne("SELECT Val FROM AuthTable WHERE Auth='SMSAeroAPIKey'")["Val"]
+	SMSNumber = MySQLFetchOne("SELECT Val FROM AuthTable WHERE Auth='SMSNumber'")["Val"]
+	SMSAuth = SMSLogin+":"+SMSAPIKey
+	all_url = "https://"+SMSAuth+"@gate.smsaero.ru/v2"
+	
+	while True:
+		MyMasternode = requests.get("https://api.2masternodes.com/api/coin/gbx/masternode/AESIR").json()["royalty"][0]
 			
 		DateString = MySQLFetchOne("SELECT Value FROM UniversalTable WHERE Service='date'")["Value"]
 		Balance = MySQLFetchOne("SELECT Value FROM UniversalTable WHERE Service='balance'")["Value"]
 		dateformat = str(dateutil.parser.parse(MyMasternode["paidAt"], dayfirst=True).replace(tzinfo=None)).replace("-","/")
-				
-		print(DateString)
-		print(dateformat)
 
 		if (MyMasternode["paidAt"] != None) and (DateString != dateformat):
 
@@ -58,12 +62,15 @@ def main():
 			NewBalance = str(float(Balance)+float(ThisPayment))
 			MySQLWriter("UPDATE UniversalTable SET Value='"+dateformat+"' WHERE Service='date'")
 			MySQLWriter("UPDATE UniversalTable SET Value='"+NewBalance+"' WHERE Service='balance'")
+			GBXPrice = requests.get("https://api.coinmarketcap.com/v2/ticker/2200/?convert=RUB").json()["data"]["quotes"]["RUB"]["price"]
+			OutString = str(dateformat)+"\nПополнение: +"+str(ThisPayment)+"GBX ("+str(round(ThisPayment*GBXPrice,2))+" RUB)\nТекущий баланс: "+NewBalance+"GBX ("+str(round(float(NewBalance)*GBXPrice,2))+" RUB)"
+			
+			if SmsSend(OutString,SMSNumber,all_url) == True:
+				print("Успешное информирование о пополнениии за "+dateformat)
+			else:
+				print("Что-то пошло не так с информированием о пополнениии за "+dateformat)
 					
-			print(str(dateformat)+"\nПополнение: +"+str(ThisPayment)+"GBX\nТекущий баланс: "+NewBalance+"GBX")
-			print("Успешное информирование о пополнениии за "+dateformat)
-					#print("Что-то пошло не так")
-					
-		time.sleep(10)
+		time.sleep(600)
 
 if __name__ == "__main__":
 	main()
